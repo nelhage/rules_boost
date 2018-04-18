28bc57ff297b35b39845d6d29d279eb92ff6cc0b
Nelson Elhage <nelhage@nelhage.com>
2018-04-17 19:29:22 -0700

    Test and fix beast/websocket
    fixes #60

--- test/beast_test.cc
+++ test/beast_test.cc
@@ -3,6 +3,7 @@
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
 #include <boost/beast/version.hpp>
+#include <boost/beast/websocket.hpp>
 #include <boost/asio/connect.hpp>
 #include <boost/asio/ip/tcp.hpp>
 #include <cstdlib>
